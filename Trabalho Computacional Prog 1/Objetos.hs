adcfdaed8e6eba74d224910c2e923cbc16ef5ff9
Renan <renan_battlenet@hotmail.com>
2018-06-21 14:36:56 -0300

    Mais comentarios

--- Trabalho Computacional Prog 1/Objetos.hs
+++ Trabalho Computacional Prog 1/Objetos.hs
@@ -6,6 +6,22 @@
 -- Data: 29/06/2018                                                           --
 --------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------
+-- MODULO OBJETOS                                                             --
+--------------------------------------------------------------------------------
+-- Este modulo contem todas as definicoes de tipos                            --
+--------------------------------------------------------------------------------
+-- Tipos:                                                                     --
+-- - Berco : Molde do berco                                                   --
+-- - Navio : Molde do navio                                                   --
+-- - VetorAtendimento : Molde de um vetor de tempo de atendimento             --
+-- - ListaDeNavios : Molde de uma lista de navios                             --
+-- - ListaDeBercos : Molde de uma lista de bercos                             --
+-- - ListaTempoAtendimento : Molde de lista de vetor de tempo de atendimento  --
+-- - NaviosNoBerco : Molde de dupla de navios no berco                        --
+-- - NaviosAlocadosBerco : Molde de lista de dupla de navio no berco          --
+--------------------------------------------------------------------------------
+
 module Objetos 
 (Berco, 
  Navio,
@@ -16,6 +32,10 @@ module Objetos
  NaviosNoBerco,
  NaviosAlocadosBerco
  ) where
+
+--------------------------------------------------------------------------------
+-- DEFINICAO DOS TIPOS                                                        --
+--------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
 -- MOLDE DO BERCO                                                             --
@@ -34,7 +54,7 @@ type ListaDeNavios = [Navio]
 --------------------------------------------------------------------------------
 type ListaDeBercos = [Berco]
 --------------------------------------------------------------------------------
--- MOLDE DE UM VETOR DE ATENDIMENTO                                           --
+-- MOLDE DE UM VETOR DE TEMPO DE ATENDIMENTO                                           --
 --------------------------------------------------------------------------------
 type VetorAtendimento = [Int]
 --------------------------------------------------------------------------------
